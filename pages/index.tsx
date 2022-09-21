cc4b36caa793856bb01c4dc3c1c78e1543ad0e44
garronej <joseph.garrone.gj@gmail.com>
2022-09-21 11:36:41 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { useIsDarkModeEnabled } from "../useIsDarkModeEnabled";
+
+export default function Index() {
+
+    const { isDarkModeEnabled, setIsDarkModeEnabled } = useIsDarkModeEnabled();
+
+    return (
+        <>
+            <h1>isDarkModeEnabled: {isDarkModeEnabled ? "yes" : "false"}</h1>
+            <button onClick={() => setIsDarkModeEnabled(!isDarkModeEnabled)}>Toggle dark mode</button>
+        </>
+    );
+
+}
